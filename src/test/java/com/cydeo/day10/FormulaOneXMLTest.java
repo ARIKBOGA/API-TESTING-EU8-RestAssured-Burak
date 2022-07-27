af69c7a110a7285bd0e8d06ff24be645b0ac9557
BURAK ARIKBOĞA <arikbogaburak@outlook.com>
2022-07-27 14:23:32 +0300

    day10 XML path. Getting element and its attributes values.

---
+++ src/test/java/com/cydeo/day10/FormulaOneXMLTest.java
@@ -0,0 +1,46 @@
+package com.cydeo.day10;
+
+import static io.restassured.RestAssured.*;
+
+import io.restassured.http.ContentType;
+import io.restassured.path.xml.XmlPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class FormulaOneXMLTest {
+
+    @BeforeAll
+    public static void setup() {
+        baseURI = "http://ergast.com";
+        basePath = "/api/f1";
+    }
+
+    @Test
+    public void test1() {
+
+        XmlPath xmlPath = given().accept(ContentType.XML)
+                .pathParam("driver", "alonso")
+                .get("/drivers/{driver}")
+                .then().statusCode(200)
+                //.log().all()
+                .extract().response().xmlPath();
+
+        String givenName = xmlPath.getString("MRData.DriverTable.Driver.GivenName");
+        String familyName = xmlPath.getString("MRData.DriverTable.Driver.FamilyName");
+        String dateOfBirth = xmlPath.getString("MRData.DriverTable.Driver.DateOfBirth");
+        String nationality = xmlPath.getString("MRData.DriverTable.Driver.Nationality");
+        System.out.println("givenName = " + givenName);
+        System.out.println("familyName = " + familyName);
+        System.out.println("dateOfBirth = " + dateOfBirth);
+        System.out.println("nationality = " + nationality);
+
+        // get attributes  **** Use "@" sign ****
+        String driverId = xmlPath.getString("MRData.DriverTable.Driver.@driverId");
+        String code = xmlPath.getString("MRData.DriverTable.Driver.@code");
+        String url = xmlPath.getString("MRData.DriverTable.Driver.@url");
+        System.out.println("driverId = " + driverId);
+        System.out.println("code = " + code);
+        System.out.println("url = " + url);
+    }
+}
---
+++ src/test/java/com/cydeo/day10/SpartanWithXML.java
@@ -0,0 +1,52 @@
+package com.cydeo.day10;
+
+import com.cydeo.utilities.SpartanAuthTestBase;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.path.xml.XmlPath;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+
+public class SpartanWithXML extends SpartanAuthTestBase {
+
+    @DisplayName("GET request to api/spartans and verify XML")
+    @Test
+    public void test1(){
+        RestAssured.given().accept(ContentType.XML)
+                .auth().basic("admin","admin")
+                .get("/api/spartans")
+                .then()
+                .statusCode(200)
+                .contentType("application/xml;charset=UTF-8")
+                .body("List.item[0].name",is("Meade"))
+                .body("List.item[0].gender",is("Male"))
+                .log().all();
+
+    }
+    @DisplayName("GET request to api/spartans with XML path")
+    @Test
+    public void test2(){
+        XmlPath xmlPath = RestAssured.given().accept(ContentType.XML)
+                .auth().basic("admin", "admin")
+                .get("/api/spartans")
+                .then()
+                .extract().xmlPath();
+
+        // get second spartan name
+        String secondName = xmlPath.getString("List.item[1].name");
+        System.out.println("secondName = " + secondName);
+
+        // get fifth phone number
+        long phone = xmlPath.getLong("List.item[4].phone");
+        System.out.println("phone = " + phone);
+
+        // get all names
+        List<Object> nameList = xmlPath.getList("List.item.name");
+        System.out.println("nameList = " + nameList);
+
+    }
+}
